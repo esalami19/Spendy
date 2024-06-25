0d02fae6cc53fa271483adfbf16efc6d3943c02e
esalami19 <esalami19@yahoo.co.uk>
2024-06-25 01:04:27 +0100

    button event listener test

---
+++ HomePage/script.js
@@ -0,0 +1,12 @@
+// const homeButtons = document.getElementById("home buttons")
+const test = document.getElementById("test")
+
+// homeButtons.addEventListener("onmouseover", e => {
+//     homeButtons.style.backgroundColor = "pink"
+//     console.log("working")
+// })
+
+test.addEventListener("click", e => {
+    // test.style.backgroundColor = "pink"
+    console.log("working")
+})
---
+++ HomePage/script.ts
@@ -0,0 +1,3 @@
+// const homeButtons: Element|null = document.querySelector("home buttons")
+
+// homeButtons.addEventListener("hover", e => {})
